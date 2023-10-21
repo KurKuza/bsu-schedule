36ba6aa699eb44a66d82873a6143818a865e352f
Danil Sherbakov <kurkuliogo@gmail.com>
2023-10-21 17:32:23 +0300

    refactored

---
+++ src/widgets/header/ui/drawer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+}
+</script>
+
+<template>
+  <v-navigation-drawer location="right">
+    <div class="drawer">
+      <v-btn @click="toggleTheme">Toggle Theme</v-btn>
+    </div>
+  </v-navigation-drawer>
+</template>
+
+<style scoped lang="scss">
+.drawer {
+  padding: 1rem;
+}
+</style>
--- src/widgets/header/ui/index.vue
+++ src/widgets/header/ui/index.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { OnClickOutside } from '@vueuse/components';
-import { useTheme } from 'vuetify';
-
+import Drawer from './drawer.vue';
 const isSearch = ref(false);
 const drawer = ref(false);
 
 const handleSearch = () => {
   isSearch.value = !isSearch.value;
 };
-
-const theme = useTheme();
-
-function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
-}
 </script>
 
 <template>
@@ -23,29 +16,26 @@ function toggleTheme() {
       <div class="header">
         <v-card-title class="pa-0">Расписание</v-card-title>
 
+        <OnClickOutside v-if="isSearch" @trigger="handleSearch">
+          <v-autocomplete
+            solo
+            flat
+            no-filter
+            dense
+            hide-details
+            variant="outlined"
+            ref="isSearch"
+            class="search"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            :append-icon="''"
+            density="compact"
+          />
+        </OnClickOutside>
         <div class="icons">
-          <div class="search-container">
-            <v-btn icon size="small" v-if="!isSearch" @click="handleSearch">
-              <v-icon icon="mdi-magnify" size="large"
-            /></v-btn>
-
-            <OnClickOutside v-else @trigger="handleSearch">
-              <v-autocomplete
-                solo
-                flat
-                no-filter
-                dense
-                hide-details
-                variant="outlined"
-                ref="isSearch"
-                class="search"
-                placeholder="Search"
-                prepend-inner-icon="mdi-magnify"
-                :append-icon="''"
-                density="compact"
-              />
-            </OnClickOutside>
-          </div>
+          <v-btn icon size="small" v-if="!isSearch" @click="handleSearch">
+            <v-icon icon="mdi-magnify" size="large"
+          /></v-btn>
 
           <v-btn icon size="small"><v-icon icon="mdi-star-outline" size="large" /></v-btn>
           <v-btn @click.stop="drawer = !drawer" icon size="small"
@@ -54,11 +44,8 @@ function toggleTheme() {
         </div>
       </div>
     </v-main>
-    <v-navigation-drawer v-model="drawer" location="right">
-      <div class="drawer">
-        <v-btn @click="toggleTheme">Toggle Theme</v-btn>
-      </div>
-    </v-navigation-drawer>
+
+    <Drawer v-model="drawer" />
   </v-layout>
 </template>
 
@@ -69,13 +56,9 @@ function toggleTheme() {
   justify-content: space-between;
   padding: 12px 16px;
 }
-.drawer {
-  padding: 1rem;
-}
 .search {
-  width: 200px;
-  padding: 0 !important;
   height: 40px !important;
+  width: 300px;
 }
 
 .icons {
