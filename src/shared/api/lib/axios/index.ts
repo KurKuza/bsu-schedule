915de82c8562474f9f3cb91957ba2aae4f92576d
Danil Sherbakov <kurkuliogo@gmail.com>
2023-11-14 22:11:14 +0300

    Added webStorage for axios

--- src/shared/api/lib/axios/index.ts
+++ src/shared/api/lib/axios/index.ts
@@ -1,12 +1,13 @@
-/* eslint-disable no-console */
 import axios from 'axios';
 import { BASE_URL } from './config';
 
-import { setupCache } from 'axios-cache-interceptor';
+import { buildWebStorage, setupCache } from 'axios-cache-interceptor';
 
 const apiAxios = axios.create({
   baseURL: BASE_URL,
 });
 
 // export const apiInstance = apiAxios;
-export const apiInstance = setupCache(apiAxios, { cacheTakeover: true, debug: console.log });
+export const apiInstance = setupCache(apiAxios, {
+  storage: buildWebStorage(localStorage, 'axios-cache:'),
+});
