9e13a8533b68b9a70fc95c0b1c7561c78e838688
Danil Sherbakov <kurkuliogo@gmail.com>
2023-11-19 19:11:40 +0300

    # This is a combination of 22 commits. # This is the 1st commit message:
    fix cors
    
    # This is the commit message #2:
    
    wip request
    
    # This is the commit message #3:
    
    try fix vercel build
    
    # This is the commit message #4:
    
    fix proxy vite
    
    # This is the commit message #5:
    
    change axios path
    
    # This is the commit message #6:
    
    allow cors vercel
    
    # This is the commit message #7:
    
    try fix cors
    
    # This is the commit message #8:
    
    fix proxy
    
    # This is the commit message #9:
    
    fix vercel cors
    
    # This is the commit message #10:
    
    fix source vercel
    
    # This is the commit message #11:
    
    remove img public
    
    # This is the commit message #12:
    
    add env
    
    # This is the commit message #13:
    
    reverse vercel source
    
    # This is the commit message #14:
    
    fix base url
    
    # This is the commit message #15:
    
    fix base url
    
    # This is the commit message #16:
    
    change vercel source
    
    # This is the commit message #17:
    
    fix vercel
    
    # This is the commit message #18:
    
    fix vercel
    
    # This is the commit message #19:
    
    fix vite proxy
    
    # This is the commit message #20:
    
    fix vite proxy
    
    # This is the commit message #21:
    
    fix axios config
    
    # This is the commit message #22:
    
    fix vercel

--- src/shared/api/lib/axios/config.ts
+++ src/shared/api/lib/axios/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = '/api/';
+export const BASE_URL = import.meta.env.VITE_PRODUCTION_API || 'bsu-api/';
--- vite.config.ts
+++ vite.config.ts
@@ -9,34 +9,19 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
-  // preview: {
-  //   host: 'localhost',
-  //   port: 5173,
-  //   strictPort: true,
-  //   headers: {
-  //     'Access-Control-Allow-Origin': 'http://localhost:5173',
-  //     'Access-Control-Allow-Headers': 'http://localhost:5173',
-  //   },
-  // },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://beluni.ru/',
+      '^.*bsu-api': {
+        target: 'https://beluni.ru',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, 'https://beluni.ru/'),
+        rewrite: (path) => path.replace(/.*bsu-api\//, 'https://beluni.ru/'),
       },
     },
-    // proxy: {
-    //   '^/(bsu|schedule)': { target: 'https://beluni.ru' },
-    // },
-    // headers: {
-    //   // 'Cross-Origin-Embedder-Policy': 'require-corp',
-    //   // 'Cross-Origin-Opener-Policy': 'cross-origin',
-    //   // 'Cross-Origin-Resource-Policy': 'cross-origin',
-    //   // 'Access-Control-Allow-Headers': 'cross-origin',
-    //   'Access-Control-Allow-Origin': 'http://localhost:5173',
-    //   'Access-Control-Allow-Headers': 'http://localhost:5173',
-    // },
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'cross-origin',
+    },
   },
 });
