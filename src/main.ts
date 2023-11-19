d5faf16e715a26b4aa1cd9a8f6570eda5b4c6d4b
Danil Sherbakov <kurkuliogo@gmail.com>
2023-11-19 18:27:57 +0300

    fix vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from './app/App.vue';
 
 import { pinia } from './shared/pinia';
 import { vuetify } from './shared/vuetify';
-import { VDatePicker } from 'vuetify/lib/labs/components.mjs';
+import { VDatePicker } from 'vuetify/labs/VDatePicker';
 
 createApp(App).use(vuetify).component('VDatePicker', VDatePicker).use(pinia).mount('#app');
