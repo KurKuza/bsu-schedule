d3e146f4263ed92918b08fe29275aaf190f0795a
Danil Sherbakov <kurkuliogo@gmail.com>
2023-11-19 18:25:31 +0300

    fix vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './app/App.vue';
 
 import { pinia } from './shared/pinia';
 import { vuetify } from './shared/vuetify';
+import { VDatePicker } from 'vuetify/lib/labs/components.mjs';
 
-createApp(App).use(vuetify).use(pinia).mount('#app');
+createApp(App).use(vuetify).component('VDatePicker', VDatePicker).use(pinia).mount('#app');
--- src/shared/vuetify/lib/index.ts
+++ src/shared/vuetify/lib/index.ts
@@ -1,15 +1,12 @@
-import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import { VDatePicker } from 'vuetify/labs/VDatePicker';
 import { en, ru } from 'vuetify/locale';
 import 'vuetify/styles';
 
 export const vuetify = createVuetify({
   components: {
     ...components,
-    VDatePicker,
   },
   directives,
   icons: {
